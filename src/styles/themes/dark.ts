a64f22e5e27a291779bb01ad4bac4135b7ec83e6
Bruno Ribeiro <the_bruno15@hotmail.com>
2021-03-02 22:52:39 -0300

    tema dark

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#18020c',
+    secundary: '#634b66',
+
+    background: '#222',
+    text: "#fff"
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,10 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#bbcbcb',
+    secundary: '#9590a8',
+    background: '#f5f5f5',
+    text: "#634B66"
+  }
+}
